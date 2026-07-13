f130f10c2fed5f4eda63328a7f90910a1125555b
arielaycen <arielaychen@gmail.com>
2026-07-13 16:53:46 +0800

    Add initial implementation of class1-1.py with basic print statements and comments

---
+++ pages/class1-1.py
@@ -0,0 +1,18 @@
+print('Hello World!')
+print('Ariel 陳安穎')
+print('不要低頭\n雙下巴會出來')
+
+"""
+這是多行註解
+"""
+
+#這是單行註解
+print('Hello World!') #print是在終端機顯示文字的指令
+# ctrl+?可快速註解/取消註解
+ 
+# 基本形態
+print(1.0 ) # float 這是浮點數
+print(1.234 ) # float 這是浮點數
+print("apple") # str 這是字串
+print(True) # bool 這是布林值
+print(False) # bool 這是布林值
