425d5c2e2935e91712998984dcca28dafbf4cfd0
arielaycen <arielaychen@gmail.com>
2026-07-13 16:59:44 +0800

    Enhance class1-1.py with additional examples for variables, operators, and string formatting

--- pages/class1-1.py
+++ pages/class1-1.py
@@ -16,3 +16,35 @@ print(1.234 ) # float 這是浮點數
 print("apple") # str 這是字串
 print(True) # bool 這是布林值
 print(False) # bool 這是布林值
+
+# 變數
+a=10 # 新增一個儲存空間並取名為a，"="的功能是將右邊的值10存入左邊的a
+print(a) # 在終端機顯示a所存的值
+a="apple" # 將a的值改為"apple"
+print(a) # 在終端機顯示a所存的值
+# 運算子
+print(1+1) # 加法
+print(1-1) # 減法   
+print(1*1) # 乘法
+print(1/1) # 除法       
+print(1//1) # 取商
+print(1%1) # 取餘數
+print(1**1) # 次方
+
+# # 優先順序
+# 1. () 括號
+# 2. ** 次方
+# 3. * / // % 乘 除 取商 取餘數
+# 4. + - 加 減
+
+# 字串運算 , + 、 *
+print("apple" + "pen") # 字串相加
+print("apple" * 3) # 字串重複   
+
+# 字串格式化
+name = "apple"
+age = 18
+print(f"Hello, my name is {name}, I'm {age} years old.")  # f-string
+# 可以將變數或其他型態的資料放到f字串裡面的{}，這樣就可以在字串中顯示
+
+
